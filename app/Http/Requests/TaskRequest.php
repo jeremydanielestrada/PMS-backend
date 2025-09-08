2a999e60924996e6eda70df1bc39110141d6db69
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-09-08 20:12:53 +0800

    Feat: created TaskRequest validation

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -23,11 +23,30 @@ class TaskRequest extends FormRequest
     {
 
 
-        if(request()->routeIs('task.store')){
-            return [
+        if(request()->routeIs('tasks.store')){
 
+            return [
+                    'project_id'   => 'required|exists:projects,id',
+                    'title'        => 'required|string|max:255',
+                    'description'  => 'nullable|string|max:255',
+                    'status'       => 'required|string|max:255',
+                    'priority'     => 'required|string|max:255',
+                    'due_date'     => 'nullable|date|after_or_equal:today',
                   ];
+        }else if(request()->routeIs('tasks.update')){
+            return [
+                    'project_id'   => 'sometimes|exists:projects,id',
+                    'title'        => 'sometimes|required|string|max:255',
+                    'description'  => 'sometimes|nullable|string|max:255',
+                    'status'       => 'sometimes|required|string|max:255',
+                    'priority'     => 'sometimes|required|string|max:255',
+                    'due_date'     => 'sometimes|nullable|date|after_or_equal:today',
+                  ];
+
         }
+
+
+        return[];
 
 
     }
