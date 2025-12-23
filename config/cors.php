c99587c4af761bd46fbba8ddeffbbf0bc7296fc0
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-12-23 18:51:43 +0800

    Initialize github actions for deployment

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', env('FRONTEND_URL', 'https://pms-frontend-chi-five.vercel.app')],
 
     'allowed_origins_patterns' => [],
 
